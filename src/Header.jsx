5012a699e16e988cfdfa452f373595406b22c37f
Nithinkashy <32662608+Nithinkashy@users.noreply.github.com>
2025-07-17 15:50:27 +0100

    changes to logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,8 @@
+import myImage from './assets/chef_icon.png';
 export default function Header(){
     return(
         <header className="header">
-            <img className="logo" src="src/assets/pngtree-chef-line-icon-png-image_6620834.png" alt="logo"  />
+            <img className="logo" src={myImage} alt="logo"  />
             <h1 className="name">Chef Nithin</h1>
         </header>
     )
